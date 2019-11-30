25399b0881a7eaeeee2972b2c0379177159ed90f
Val <33302737+ValWV@users.noreply.github.com>
2019-11-30 08:00:10 +0300

    Update index.js

--- docs/parse/index.js
+++ docs/parse/index.js
@@ -19,7 +19,8 @@ function getWaitTimes(){
         var htmlOutPut = ''+
           '<div class="col-md-3 card card-shadow">'+
             '<div class="card__top">'+
-            '<img class="card-img-top" src="'+totalPatients+'"></div>'+
+            '<img class="card-img-top" src="'+totalPatients+'">'+
+            '</div>'+
             '<div class="waitTimes__address">'+departmentName+'</div>'+
             '<div class="waitTimes__information">'+
               '<div class="waitTimes__approximateWaitTimes">APPROXIMATE<br />WAIT TIMES</div>'+
