dd5dcdbd18a29b9b6af7695b013fdfff12b4293f
Val <33302737+ValWV@users.noreply.github.com>
2019-12-01 11:24:12 +0300

    Update parse.js

--- docs/parse/parse.js
+++ docs/parse/parse.js
@@ -7,7 +7,7 @@ function getWaitTimes(){
     success: function(xml){
       $xml = $(xml);
       $($xml).find('realty-feed>offer').each(function(){
-        var id = $(this).find('id').eq(0).text();
+        var id = $(this).attr('internal-id');
         var idType = $(this).find('type').eq(0).text();
         var id2 = $(this).find('ID').eq(1).text();
         var idType2 = $(this).find('Type').eq(1).text();
@@ -15,26 +15,92 @@ function getWaitTimes(){
         var averageWaitTime = Math.round($(this).find('AverageWaitTime').text()/60);
         var longestWait = Math.round($(this).find('LongestCurrentWaitTime').text()/60);
         var price = $(this).find('price>value').text();
-        var totalPatients = $(this).find('image[tag="plan"]:first').text();
+        var readyquarter = $(this).find('ready-quarter').text();
+        var area = $(this).find('area>value').text();
+        var lspace = $(this).find('living-space>value').text();
+        var kspace = $(this).find('kitchen-space>value').text();
+        var floor = $(this).find('floor').text();
+        var floorstotal = $(this).find('floors-total').text();
+        var photo = $(this).find('image[tag="plan"]:first').text();
+        var buildingname = $(this).find('building-name').text();
+        var buildingsection = $(this).find('building-section').text();
+        var builtyear = $(this).find('built-year').text();
+        var rooms = $(this).find('rooms').text();
+        
+        
         var htmlOutPut = ''+
-          '<div class="col-md-3 card card-shadow">'+
+          '<div class="col-md-3" data-id="'+id+'">'+
+            '<div class="card card-2" data-toggle="modal" data-target="#item'+id+'">'+
             '<div class="card__top">'+
-                      '<img class="card-img-top" src="'+totalPatients+'">'+
+                      '<img class="card-img-top" style="width:285px; height:250px;" src="'+photo+'">'+
             '</div>'+
-            '<h4>'+Name+'</h4>'+
-            '<div class="card__body">'+
-              '<h5 data-animation="animated flipInX" class="m-b-10 m-t-0">'+price+'</h5>'+
-              '<div class="text-info m-b-0">'+longestWait+'</div>'+
-              '<div class="waitTimes__minutes">minutes</div>'+              
-            '</div>'+
-            '<div class="waitTimes__other">id: '+id+'</div>'+
-            '<div class="waitTimes__other">id type: '+idType+'</div>'+
-            '<div class="waitTimes__other">id2: '+id2+'</div>'+
-            '<div class="waitTimes__other">id type2: '+idType2+'</div>'+
-            '<div class="bg-light slide-content specifiaction-box"> <div class="row"> '+
-            '<div class="col-6">longest wait: '+longestWait+'</div>'+
-            '<div class="col-6">patients waiting: </div></div></div>'+
             
+            '<div class="card__body">'+
+              '<h4>'+Name+'</h4>'+
+              '<div class="text-info m-b-0">'+price+'RUB</div>'+          
+            '</div>'+
+            '<div class="card__bottom text-center">'+
+            '<div class="card__action">'+
+                '<span class="specifiaction text-muted">Срок сдачи</span>'+
+                '<div class="m-b-0 font-medium">'+readyquarter+'</div>'+
+            '</div>'+
+            '<div class="card__action">'+
+                '<span class="specifiaction text-muted">Общая площадь</span>'+
+                '<div class="m-b-0 font-medium">'+area+'</div>'+
+            '</div>'+
+            '<div class="card__action">'+
+                '<span class="specifiaction text-muted">Жилья площадь</span>'+
+                '<div class="m-b-0 font-medium">'+lspace+'</div>'+
+            '</div>'+
+            '<div class="card__action">'+
+                '<span class="specifiaction text-muted">Этаж</span>'+
+                '<div class="m-b-0 font-medium">'+floor+'из'+floorstotal+'</div>'+
+            '</div>'+
+            '</div>'+
+         
+          '</div>'+
+          '<div id="item'+id+'" class="modal fade custom-modal modal5" tabindex="-1" role="dialog" style="display: none;" aria-hidden="true">'+
+                                    '<div class="modal-dialog modal-lg">'+
+                                        '<div class="modal-content">'+  
+                                           '<div class="modal-header">'+
+                                               '<a href="#" class="close-btn" data-dismiss="modal" aria-hidden="true">×</a>'+
+                                            '</div>'+                                                       
+                                          '<div class="modal-body p-40 text-justify">'+
+                                          '<div class="row">'+
+                                            '<div class="col-lg-4 col-md-4">'+ 
+                                                 '<img class="rounded img-responsive" src="'+photo+'" alt="">'+
+                                            '</div>'+
+                                             '<div class="col-lg-8 col-md-8">'+
+                                                        '<div class="modal-bg">'+
+                                                               '<h4 class="modal-title">АПАРТАМЕНТЫ  ' +buildingname + rooms  +'</h4>'+
+                                                    '<div class="row">'+    
+                                                         '<div class="col-md-6">'+ 
+                                                                '<h5 class="m-b-20 text-uppercase font-semibold">'+ area +'м2</h5>'+           
+                                                          '</div>'+
+                                                          '<div class="col-md-6">'+ 
+                                                                '<h5 class="m-b-20 text-uppercase font-semibold">'+ price +'RUB</h5>'+           
+                                                          '</div>'+
+                                                     '</div>'+
+            
+                                                    '<div class="row">'+    
+                                                         '<div class="col-md-4">'+ 
+                                                                '<h6 class="m-b-20 text-uppercase font-semibold">'+ lspace +'м2</h6>'+           
+                                                          '</div>'+
+                                                          '<div class="col-md-4">'+ 
+                                                                '<h6 class="m-b-20 text-uppercase font-semibold">'+ kspace +'м2</h6>'+           
+                                                          '</div>'+
+                                                          '<div class="col-md-4">'+ 
+                                                                '<h6 class="m-b-20 text-uppercase font-semibold">'+ floor +'</h6>'+           
+                                                          '</div>'+
+                                                     '</div>'+            
+            
+                                              '</div>'+
+                                            '</div>'+
+                                            '</div>'+
+                                          '</div>'+
+                                      '</div>'+
+                                '</div>'+
+          
           '</div>'+
         '';
         $('.data__sims').append(htmlOutPut);
@@ -45,7 +111,6 @@ function getWaitTimes(){
     }
   });
 }
-
 $(document).ready(function() {
   getWaitTimes();
 });
